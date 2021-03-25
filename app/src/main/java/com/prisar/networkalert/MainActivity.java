06971356020b4de0a5fc333459c968e2c66d114d
prisar <pritimaysarkar2010@gmail.com>
2021-03-25 20:48:02 +0530

    add app status class

--- app/src/main/java/com/prisar/networkalert/MainActivity.java
+++ app/src/main/java/com/prisar/networkalert/MainActivity.java
@@ -2,7 +2,12 @@ package com.prisar.networkalert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (AppStatus.getInstance(this).isOnline()) {
+
+            Toast.makeText(this,"You are online!!!!", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            Toast.makeText(this,"You are not online!!!!",Toast.LENGTH_SHORT).show();
+            Log.v("Home", "############################You are not online!!!!");
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
